6d09681be35d4b790a7562ec76a607e07a57953e
Ben-Bingham <ben@benbingham.ca>
2025-03-21 19:23:10 -0600

    Create Buffer.h

---
+++ src/Utilities/OpenGl/Buffer.h
@@ -0,0 +1,38 @@
+#pragma once
+
+#include <vector>
+#include <iostream>
+
+#include <gl/glew.h>
+#include <GLFW/glfw3.h>
+
+template<typename T, int BUFFER_TYPE>
+class GlBuffer {
+public:
+    GlBuffer(const std::vector<T>& data, unsigned int drawMode = GL_STATIC_DRAW) {
+        glGenBuffers(1, &handle);
+
+        Bind();
+
+        glBufferData(BUFFER_TYPE, data.size() * sizeof(T), (void*)data.data(), drawMode);
+    }
+
+    GlBuffer(const GlBuffer& other) = delete;
+    GlBuffer(GlBuffer&& other) noexcept = default;
+    GlBuffer& operator=(const GlBuffer& other) = delete;
+    GlBuffer& operator=(GlBuffer&& other) noexcept = default;
+
+    ~GlBuffer() {
+        glDeleteBuffers(1, &m_Handle);
+    }
+
+    void Bind() {
+        glBindBuffer(BUFFER_TYPE, m_Handle);
+    }
+
+private:
+    unsigned int m_Handle{ 0 }
+};
+
+using VBO = GlBuffer<float, GL_ARRAY_BUFFER>;
+using EBO = GlBuffer<unsigned int, GL_ELEMENT_ARRAY_BUFFER>;
